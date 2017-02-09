5feefc05c8dbf0326678c5e96023dddcea1e9415
Stefan Ullinger <stefan@slicemenice.de>
2017-02-09 12:30:20 +0100

    Added latest d.ts files.

--- sources/abstractOffCanvasService.d.ts
+++ sources/abstractOffCanvasService.d.ts
@@ -4,31 +4,47 @@ export interface OffCanvasView {
 }
 export interface IOffCanvasService {
     activateView(view: OffCanvasView): void;
-    addTransitionCallback(from: string, to: string, callback: {
+    addTransitionCallback(from: string, to: string, transitionCallback: {
         (prevView: OffCanvasView, nextView: OffCanvasView): Promise<void>;
+    }, skipOrCleanupCallback?: {
+        (prevView: OffCanvasView, nextView: OffCanvasView): void;
     }): void;
-    changeView(prevView: OffCanvasView, nextView: OffCanvasView): Promise<void>;
-    dismissCurrentView(): Promise<void>;
+    changeView(prevView: OffCanvasView, nextView: OffCanvasView, replace: boolean, skipTransitions: boolean): Promise<void>;
+    dismissCurrentView(skipTransitions: boolean): Promise<void>;
     fixateView(view: OffCanvasView): void;
-    isShowingOffCanvasView(viewIdentifier?: string): boolean;
+    getNumberOfViewsOnViewstack(): number;
+    getRegisteredViews(): Array<OffCanvasView>;
+    isShowingView(viewIdentifier?: string): boolean;
     registerView(viewIdentifier: string, element: HTMLElement): OffCanvasView;
+    replaceCurrentViewWith(viewIdentifier: string, skipTransitions: boolean): void;
     setBaseView(view: OffCanvasView): void;
-    showView(viewIdentifier: string): void;
+    showView(viewIdentifier: string, skipTransitions: boolean): void;
+    unregisterView(viewIdentifier: string): void;
 }
 export declare abstract class AbstractOffCanvasService implements IOffCanvasService {
     protected baseView: OffCanvasView;
-    protected registeredViews: any;
-    protected transitionCallbacks: Map<string, ((prevView: OffCanvasView, nextView: OffCanvasView) => Promise<void>)[]>;
+    protected registeredViews: Map<string, OffCanvasView>;
+    protected transitionCallbacks: Map<string, {
+        transitionCallback: (prevView: OffCanvasView, nextView: OffCanvasView) => Promise<void>;
+        skipOrCleanupCallback: (prevView: OffCanvasView, nextView: OffCanvasView) => void;
+    }[]>;
     protected viewStack: OffCanvasView[];
     abstract activateView(view: OffCanvasView): void;
-    addTransitionCallback(from: string, to: string, callback: {
+    addTransitionCallback(from: string, to: string, transitionCallback: {
         (prevView: OffCanvasView, nextView: OffCanvasView): Promise<void>;
+    }, skipOrCleanupCallback?: {
+        (prevView: OffCanvasView, nextView: OffCanvasView): void;
     }): void;
-    abstract changeView(prevView: OffCanvasView, nextView: OffCanvasView): Promise<void>;
-    dismissCurrentView(): Promise<void>;
+    abstract changeView(prevView: OffCanvasView, nextView: OffCanvasView, replace: boolean, skipTransitions: boolean): Promise<void>;
+    dismissCurrentView(skipTransitions?: boolean): Promise<void>;
     abstract fixateView(view: OffCanvasView): void;
-    isShowingOffCanvasView(viewIdentifier?: string): boolean;
+    getNumberOfViewsOnViewstack(): number;
+    getRegisteredViews(): OffCanvasView[];
+    isShowingView(viewIdentifier?: string): boolean;
+    isTopmostView(viewIdentifier: string): boolean;
     registerView(viewIdentifier: string, element: HTMLElement): OffCanvasView;
+    replaceCurrentViewWith(viewIdentifier: string, skipTransitions?: boolean): void;
     setBaseView(view: OffCanvasView): void;
-    showView(viewIdentifier: string): void;
+    showView(viewIdentifier: string, skipTransitions?: boolean): void;
+    unregisterView(viewIdentifier: string): void;
 }
